b66fc00c2501c475d2a6264fe0583bc9e0667280
khareyash05 <khareyash05@gmail.com>
2023-02-09 16:59:39 +0530

    added topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css'
+import { ColorModeContext , useMode } from './theme'
+import { CssBaseline , ThemeProvider } from '@mui/material'
+import TopBar from './scenes/globals/TopBar'
 
 function App() {
-
+  const [theme,ColorMode] = useMode()
   return (
-    <div className="app">
-    </div>
+    <ColorModeContext.Provider value={ColorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <div className="app">
+          <main className='content'>
+            <TopBar/>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
 
